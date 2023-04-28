0aa06ea5e13b46751839c9358b452d387032cc1e
MariaPinguelli <mariaferpinguelli@hotmail.com>
2023-04-28 16:16:13 -0300

    model

---
+++ pkg/model/model.go
@@ -0,0 +1,17 @@
+package model
+
+type Frase struct{
+	tamanho string
+	verboso string
+	nulo bool
+}
+
+type Texto struct{
+	verboso string
+	nulo bool
+}
+
+type Data struct{
+	verboso string
+	nulo bool
+}
