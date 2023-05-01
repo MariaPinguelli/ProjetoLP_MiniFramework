b9e741ec3abc6b4ef03644689ac3f84c8b4c22e9
MariaPinguelli <mariaferpinguelli@hotmail.com>
2023-05-01 10:15:49 -0300

    test.go

---
+++ test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/MariaPinguelli/ProjetoLP_MiniFramework/pkg/model"
+	"github.com/MariaPinguelli/ProjetoLP_MiniFramework/pkg/generator"
+)
+
+var Data []interface{}
+
+func main(){
+	Data = append(Data, model.NovaFrase(255, "FraseTeste", true))
+	Data = append(Data, model.NovoTexto("TextoTeste", false))
+	Data = append(Data, model.NovaData(true))
+
+	generator.GenerateHTMLFields(Data)
+	
+	fmt.Println(Data)
+}
